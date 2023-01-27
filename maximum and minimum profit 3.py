f4e6f8d6df5526e18e80e105771e1d3e191a4579
NandurkarNeil <87815475+Vishal220796@users.noreply.github.com>
2023-01-27 19:40:23 +0530

    Add files via upload

---
+++ maximum and minimum profit 3.py
@@ -0,0 +1,25 @@
+month = ("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December")
+ 
+profits = (2000, 45000, 78000, 997000, 12000, 456000, 65000, 54000, 10000, 30000, 70000, 80000)
+ 
+max_profit = max(profits)
+max_profit_index = profits.index(max_profit)
+print(max_profit_index)
+ 
+ 
+ 
+max_profit_month = month[max_profit_index]
+print("The maximum profit of " + str(max_profit)+ " was recorded in the month of "
+       + str(max_profit_month))
+ 
+ 
+ 
+min_profit = min(profits)
+min_profit_index = profits.index(min_profit)
+print(min_profit_index)
+ 
+ 
+ 
+min_profit_month = month[min_profit_index]
+print( "The minimum profit of "+str(min_profit)+" was recorded in the month of "
+        +str(min_profit_month))
